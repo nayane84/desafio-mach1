f28365d74f52bff744b7c19f9d055f8808d8a5a1
muellerviana <muellerviana@gmail.com>
2022-06-03 14:24:07 -0300

    version 1.0 released

--- js/table-script.js
+++ js/table-script.js
@@ -4,12 +4,13 @@ function cadAluno(nome, cpf, email, curso) {
     var qtdLinhas = tbl.rows.length;
     var linha = tbl.insertRow(qtdLinhas);
 
+
     var cellId = linha.insertCell(0);
     var cellNome = linha.insertCell(1);
     var cellCpf = linha.insertCell(2);
     var cellEmail = linha.insertCell(3);
     var cellCurso = linha.insertCell(4);
-   
+
 
     cellId.innerHTML = qtdLinhas;
     cellNome.innerHTML = nome;
